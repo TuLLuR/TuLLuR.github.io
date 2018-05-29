8a2b41508e083a026e6332f468aa78567cef1eca
TuLLuR <tullur69@mail.ru>
2018-05-29 12:59:59 +0300

    yikes

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,3 @@
-const gulp = require('gulp');
-const watch = require('gulp-watch');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-
-gulp.task('default', () => {
-    console.log('You create it!');    
-});
-
-gulp.task('html', () => {
-    console.log('HTML');
-});
-
-gulp.task('css', () => {
-    console.log('CSS');
-});
-gulp.task('watch', () => {
-    watch('./app/index.html', () => {
-        gulp.start('html');
-    });
-
-    watch('./app/style/**/*.css', () => {
-        gulp.start('css');
-    });
-});
